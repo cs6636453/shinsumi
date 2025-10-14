c624a782a67caa868a56dad6dcd89e583fbed759
Phongsiri Loedphongthai <wntv2020@gmail.com>
2025-10-14 17:26:35 +0700

    14/10/2025 - Backup from local to git

---
+++ gadget_store/lib/connect.php
@@ -0,0 +1,12 @@
+<?php
+try {
+    $pdo = new PDO("mysql:host=localhost;
+                         dbname=168DB_59;
+                         charset=utf8","168DB59","iprPhuxf");
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//    echo "Connection successfully";
+}
+catch (PDOException $e) {
+    echo "Connection failed: (read below)<br>" . $e -> getMessage();
+}
+?>
