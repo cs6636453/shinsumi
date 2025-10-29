4eba6b9397f562376f80eb43882281ab9f4f06d6
Phongsiri Loedphongthai <wntv2020@gmail.com>
2025-10-29 13:09:24 +0700

    1

---
+++ gadget_store/search/ajax.js
@@ -0,0 +1,20 @@
+let queryXML;
+
+function queryXMLfunc() {
+    queryXML = new XMLHttpRequest();
+    queryXML.onreadystatechange = showQueryXMLfunc;
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTerm = urlParams.get('search');
+    let params = "search=" + encodeURIComponent(searchTerm);
+    let url = "query.php";
+    queryXML.open("POST", url);
+    queryXML.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    queryXML.send(params);
+}
+function showQueryXMLfunc() {
+    if (queryXML.readyState == 4 && queryXML.status == 200) {
+        document.getElementById("queryResult").innerHTML = queryXML.responseText;
+    }
+}
+
+queryXMLfunc();
