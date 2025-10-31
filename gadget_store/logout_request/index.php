30771fde3a94c19d7291acd8f250eefd1d00a667
Phongsiri Loedphongthai <wntv2020@gmail.com>
2025-10-31 10:01:09 +0700

    session ID

--- gadget_store/logout_request/index.php
+++ gadget_store/logout_request/index.php
@@ -1,6 +1,7 @@
 <html><body><p>กำลังออกจากระบบ...</p></body></html>
 <?php
     include "../db/connect.php";
+    unset($_SESSION['username']);
     session_destroy();
     header("location: ../");
 ?>
